97e990ec42150716847b4a3082d2138b8f51d587
Thais Bueno <tfb.bueno@gmail.com>
2019-10-30 20:34:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,38 +4,45 @@ const Telegram = require("telegram-node-bot");
 
 const { TelegramBaseController } = Telegram;
 const { TextCommand } = Telegram;
+const msg = [
+  "KOT é muito ruim!",
+  "Vai dar tudo certo... menos pra quem joga KOT!",
+  "Eu ganhei em último lugar!!!",
+  "HOJE TINHA ENCONTRO??????? @Luiz",
+  "KD o veneno?",
+  "Sayuri está Sayurando",
+  "OLHA A VELAAAAAAA!",
+  "Pq manda audio, Aline?",
+  "Vim lá do Tijucal só pra ganhar do Luiz"
+];
+
 const chatbot = new Telegram.Telegram(process.env.TOKEN);
 class KOTController extends TelegramBaseController {
   kotAction(scope) {
-    const msg = [
-      "KOT é muito ruim!",
-      "Vai dar tudo certo... menos pra quem joga KOT!",
-      "Eu ganhei em último lugar!!!",
-      "HOJE TINHA ENCONTRO??????? @Luiz",
-      "KD o veneno?",
-      "Sayuri está Sayurando",
-      "OLHA A VELAAAAAAA!",
-      "Pq manda audio, Aline?",
-      "Vim lá do Tijucal só pra ganhar do Luiz"
-    ];
-
     const index = Math.floor(Math.random() * Math.floor(msg.length));
     scope.sendMessage(msg[index]);
   }
 
-  gitAction(scope) {
-    const msg =
-      "Contribua para este belíssimo projeto! https://github.com/CUBA-dev/cubabot";
+  kotAllAction(scope) {
+    msg.forEach(element => {
+      scope.sendMessage(element);
+    });
+  }
 
-    scope.sendMessage(msg);
+  gitAction(scope) {
+    scope.sendMessage(
+      "Contribua para este belíssimo projeto! https://github.com/CUBA-dev/cubabot"
+    );
   }
 
   get routes() {
     return {
       kot: "kotAction",
+      kotAll: "kotAllAction",
       git: "gitAction"
     };
   }
 }
 chatbot.router.when(new TextCommand("/kot", "kot"), new KOTController());
+chatbot.router.when(new TextCommand("/kotAll", "kotAll"), new KOTController());
 chatbot.router.when(new TextCommand("/git", "git"), new KOTController());
